00f55b4d145fc06beabb05f95141dfc6448c7354
tmullen <tony.mullen@gmail.com>
2016-07-25 01:15:32 +0900

    through p 56

---
+++ src/scope.js
@@ -0,0 +1,49 @@
+'use strict';
+var _ = require('lodash');
+
+// function values are always unique
+function initWatchVal() { }
+
+function Scope() {
+  // $$ signifies private to Angular, not to be called from application code
+  this.$$watchers = [];
+}
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn || function() { },
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$$digestOnce = function() {
+  var self = this;
+  var newValue, oldValue, dirty;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue,
+        (oldValue === initWatchVal ? newValue : oldValue),
+        self);
+      dirty = true;
+    }
+  });
+  return dirty;
+};
+
+Scope.prototype.$digest = function() {
+  var ttl = 10;
+  var dirty;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw '10 digest iterations reached';
+    }
+  } while (dirty);
+};
+
+module.exports = Scope;
