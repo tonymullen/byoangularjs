88b164c743cb8f2963a44eed4d63804f257c9cbe
tmullen <tony.mullen@gmail.com>
2016-07-29 10:06:16 -0700

    ch 5

--- src/scope.js
+++ src/scope.js
@@ -14,6 +14,7 @@ function Scope() {
   this.$$postDigestQueue = [];
   this.$root = this;
   this.$$children = [];
+  this.$$listeners = {};
   this.$$phase = null;
 }
 
@@ -33,9 +34,85 @@ Scope.prototype.$new = function(isolated, parent) {
   }
   parent.$$children.push(child);
   child.$$watchers = [];
+  child.$$listeners = {};
   child.$$children = [];
   child.$parent = parent;
   return child;
+};
+
+Scope.prototype.$emit = function(eventName) {
+  var propagationStopped = false;
+  var event = {
+    name: eventName,
+    targetScope: this,
+    stopPropagation: function() {
+      propagationStopped = true;
+    },
+    preventDefault: function() {
+      event.defaultPrevented = true;
+    }
+  };
+  var listenerArgs = [event].concat(_.tail(arguments));
+  var additionalArgs = _.tail(arguments);
+  var scope = this;
+  do {
+    event.currentScope = scope;
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    scope = scope.$parent;
+  } while (scope && !propagationStopped);
+  event.currentScope = null;
+  return event;
+};
+
+Scope.prototype.$broadcast = function(eventName) {
+  var event = {
+    name: eventName,
+    targetScope: this,
+    preventDefault: function() {
+      event.defaultPrevented = true;
+    }
+  };
+  var listenerArgs = [event].concat(_.tail(arguments));
+  this.$$everyScope(function(scope) {
+    event.currentScope = scope;
+    scope.$$fireEventOnScope(eventName, listenerArgs);
+    return true;
+  });
+  event.currentScope = null;
+  return event;
+};
+
+Scope.prototype.$$fireEventOnScope = function(eventName, listenerArgs) {
+  var listeners = this.$$listeners[eventName] || [];
+  var i = 0;
+  while (i < listeners.length) {
+    if (listeners[i] === null) {
+      listeners.splice(i, 1);
+    } else {
+      try {
+        listeners[i].apply(null, listenerArgs);
+      } catch(e) {
+        console.error(e);
+      }
+      i++;
+    }
+  }
+
+  return event;
+};
+
+Scope.prototype.$on = function(eventName, listener) {
+  var listeners = this.$$listeners[eventName];
+  if (!listeners) {
+    this.$$listeners[eventName] = listeners = [];
+  }
+  listeners.push(listener);
+  return function() {
+    var index = listeners.indexOf(listener);
+    if (index >= 0) {
+      listeners[index] = null;
+    }
+  };
 };
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
@@ -180,6 +257,7 @@ Scope.prototype.$digest = function() {
 };
 
 Scope.prototype.$destroy = function() {
+  this.$broadcast('$destroy');
   if (this.$parent) {
     var siblings = this.$parent.$$children;
     var indexOfThis = siblings.indexOf(this);
@@ -188,6 +266,7 @@ Scope.prototype.$destroy = function() {
     }
   }
   this.$$watchers = null;
+  this.$$listeners = {};
 };
 
 Scope.prototype.$eval = function(expr, locals) {
